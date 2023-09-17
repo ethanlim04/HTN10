3c281c0f0bc820488ea54bc36561ba33b9b90dee
Ethan Lim <hyeonseong04.lim@gmail.com>
2023-09-16 21:06:56 -0400

    added video out

---
+++ yolov5/main.py
@@ -0,0 +1,5 @@
+from transformers import AutoProcessor, Blip2ForConditionalGeneration
+import torch
+
+processor = AutoProcessor.from_pretrained("Salesforce/blip2-opt-2.7b")
+model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b", torch_dtype=torch.float16)
